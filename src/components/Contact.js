e8a8e8a97ee9c478d492a30efcc15572d364670c
Mark Delgado <markdelgado.md@hotmail.com>
2023-01-27 23:32:44 -0500

    added contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,35 @@
+import './styles/Contact.css'
+
+
 const Contact = () => {
+
+
     return( 
-        <div>
-            <h1>Contact Page</h1>
+        <div className="Contact-container">
+            <h2>Contact Us</h2>
+            <form >
+                <ul>
+                    <li className='half'>
+                        <input type='text' name='name' placeholder='Name' required />
+                    </li>
+                    <li className='half'>
+                        <input type='email' name='email' placeholder='Email' required />
+                    </li>
+                    <li>
+                        <input type='text' name='subject' placeholder='Subject' required />
+                    </li>
+                    <li>
+                        <textarea placeholder='Message' name='message' required></textarea>
+                    </li>
+                    <li>
+                        <input className='flat-button' type='submit' value='Send' />
+                    </li>
+
+
+                </ul>
+
+            </form>
+
         </div>
     )
 }
