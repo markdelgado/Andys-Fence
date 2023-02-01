dc03204acbebd3d4efc6f3321b7e186f6eec23e2
Mark Delgado <markdelgado.md@hotmail.com>
2023-01-31 23:41:44 -0500

    added form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,21 @@
 import './styles/Contact.css'
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
+    const refForm = useRef()
+
+    const sendForm = (e) => {
+        e.preventDefault()
+        console.log(refForm)
+    }
 
 
     return( 
         <div className="Contact-container">
             <h2>Contact Us</h2>
-            <form >
+            <form ref={refForm} onSubmit={sendForm}>
                 <ul>
                     <li className='half'>
                         <input type='text' name='name' placeholder='Name' required />
